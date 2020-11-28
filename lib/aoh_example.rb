bb6ab498c4c1922871ffac1ea4c7bdbc49780124
esequiel medina <medinaesequiel@gmail.com>
2020-11-28 14:23:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,13 +6,14 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
+
+
 def assembled_aoh
   names_q = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G,]
 end
  
 
 def literal_aoh
-  
   
  
 end
