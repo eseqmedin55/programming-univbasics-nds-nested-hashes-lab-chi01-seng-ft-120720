44c274b0f7e28eb2c09da5a0d10ddec42eb39082
esequiel medina <medinaesequiel@gmail.com>
2020-11-28 14:29:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ end
  
 
 def literal_aoh 
-  literal_format = [{:name=> "Don Gately", :occupation=> "Live-in Staff"}, {:name=>}]
+  literal_format = [{:name=>"Don Gately", :occupation=>"Live-in Staff"}, {:name=>"Joelle van Dyne", :occupation=> "Radio Personality"}, {:name=>"Pat Monteseian", :occupation=> "Staff" }]
   
  
 end
